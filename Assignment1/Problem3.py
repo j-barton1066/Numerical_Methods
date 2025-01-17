3b54a94b6fbf8d548aa58f72d55c842267b0a0e2
J Barton <jamesgoodinbarton@gmail.com>
2025-01-17 16:49:04 -0700

    Finished Problem3

--- Assignment1/Problem3.py
+++ Assignment1/Problem3.py
@@ -12,7 +12,7 @@ list = []
 
 for i in numlist:
     list.append(int(i))
-print(list)
+#print(list)
 
 #Put list in reverse order
 list.sort(reverse=True)
@@ -21,7 +21,14 @@ print(list)
 mean = np.mean(list)
 median = np.median(list)
 mode = stats.mode(list)
+modelist = []
+modevalue = list.append(int(mode.mode))
+
+
 
 print("Mean: ", mean)
 print("Meadian: ", median)
-print("Mode: ", mode)
+if len(modelist) == 0:
+    print("Mode: All numbers are represented once")
+else:
+    print(f"Mode: {',' .join(map(str, modelist))}")
