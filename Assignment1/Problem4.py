04faa58415c7a8998fd150e1f0eeebb1379aa7e6
J Barton <jamesgoodinbarton@gmail.com>
2025-01-21 10:29:16 -0700

    Working on problem 4 fixind ODE and adding graphs

--- Assignment1/Problem4.py
+++ Assignment1/Problem4.py
@@ -3,9 +3,7 @@
 #A02298690
 import numpy as np
 import matplotlib.pyplot as mp
-from scipy.integrate import ode
 
-from Problem2 import label
 
 g = 9.8 #m/s^2
 m = 9.8 #kg
@@ -13,9 +11,12 @@ c = 49.5 #kg/s
 v_0 = 0 #m/s
 start = 0 #s
 stop = 1.5 #s
-num = 10
+num = 100
 t = np.linspace(start, stop, num)
-dt = 0.1
+#Problem B
+#dt = 0.1
+dt = .01
+
 
 #defines terminal velocity
 def terminalvelocity(g,m,c):
@@ -27,29 +28,34 @@ def velocitycalc(m,c,v_T,t):
     v = v_T  * (1 - np.exp(-(c / m) * t))
     return v
 
-# #Defines the ODE for velocity
-# def odevelocity(g,m,c,stop,dt):
-#     time = np.arange(start,stop,dt)
-#     velocity = np.zeros(len(time))
-#     for i in range(len(time)):
-#         velocity[i] = velocity[i - 1] + (g - (c/m) * velocity[i - 1]) * dt
-#     return velocity, time
+#Defines the ODE for velocity
+def odevelocity(g,m,c,stop,dt):
+    time = np.arange(start,stop,dt)
+    y = np.zeros_like(time)
+    v_0 = 0 #m/s
+    y[0] = v_0
+    for t in range(1,len(time)):
+        y[t] = y[t-1] + (g - (c/m) * y[t-1]) * dt
+    return time,y
+
 
 v_T = terminalvelocity(g,m,c)
 v = velocitycalc(m,c,v_T,t)
-print(velocitycalc(m, c, t, v_T))
-# time, velocity = odevelocity(g,m,c,stop,dt)
-# print(odevelocity(g,m,c,stop,dt))
+#debug
+#print(velocitycalc(m, c, t, v_T))
+time, y = odevelocity(g,m,c,stop,dt)
+#debug
+#print(odevelocity(g,m,c,stop,dt))
 
 #create plot and show the plot and save as png
-# mp.plot(time,velocity, label = "ODE")
+mp.plot(time,y, label = "ODE")
 mp.plot(t, v, label = "Velocity")
 mp.xlabel("Time (s)")
 mp.ylabel("Velocity (m/s)")
 mp.title("")
 mp.legend(loc='upper right')
 mp.grid()
-#plot.savefig("Radioactive_Decay.png")
+#mp.savefig("Problem4_PartC.png")
 mp.show()
 
 
