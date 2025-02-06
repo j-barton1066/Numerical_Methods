6502ef3359262453a83a81a97d4f64588393dea3
J Barton <jamesgoodinbarton@gmail.com>
2025-02-06 10:00:48 -0700

    Update to allow for 2x2 matrix

--- useful_programs/gaussian_elimination.py
+++ useful_programs/gaussian_elimination.py
@@ -11,19 +11,20 @@ def gaussian_elimination(A,b):
         for k in range(i+1, n):
             factor = A[k, i] / A[i, i]
             for j in range(i, n):
-                A[k][j] = A[k][j] - factor * A[i][j]
-                b[k] = b[k] - factor * b[i]
+                A[k, j] -= factor * A[i, j]
+                b[k] -= factor * b[i]
     #backward
     x = np.zeros(n)
     for i in range(n-1, -1, -1):
-        x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
+        #adds out of bounds condition
+        x[i] = (b[i] - np.dot(A[i, i+1:] if i+1 < n else 0, x[i+1:] if i+1 < n else 0)) / A[i, i]
     return x
 
 # A = np.array([[2,3,1], [4,1,2], [-2,5,2]], dtype=np.float64)
 # b = np.array([9,3,7], dtype=np.float64)
 
-A = np.array([[0,4,2], [2,6,1], [3,7,6]], dtype=np.float64)
-b = np.array([8,-3,5], dtype=np.float64)
+A = np.array([[2,100000],[1,1]], dtype=np.float64)
+b = np.array([100000,2], dtype=np.float64)
 print(A)
 print(b)
 solution = gaussian_elimination(A,b)
