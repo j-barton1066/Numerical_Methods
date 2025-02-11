c10de14d0ad5b9134f1489fab92420c9ba093d7e
J Barton <jamesgoodinbarton@gmail.com>
2025-02-11 12:09:19 -0700

    star assignment 2

---
+++ Assignment2/Problem2.py
@@ -0,0 +1,64 @@
+#Gauss Elimination with Partial Pivoting for an N x N matrix
+import numpy as np
+
+def create_matrix(n):
+    matrix = []
+    print(f"Enter the {n} x {n} matrix by rows: ")
+    for i in range(n):
+        while True:
+            row_list = list(map(int, input(f"Row {i+1}: ").split()))
+            if len(row_list) == n:
+                break
+            else:
+                print("Please enter a valid matrix.")
+        matrix.append(row_list)
+    matrix = np.array(matrix, dtype=float)
+    return matrix
+
+def solution_matrix(n):
+    solution = []
+    print(f"Enter the {n} x 1 matrix by rows: ")
+    for i in range(n):
+        while True:
+            row_list = list(map(int, input(f"Row {i+1}: ").split()))
+            if len(row_list) == 1:
+                break
+            else:
+                print("Please enter a valid matrix.")
+        solution.append(row_list)
+    solution = np.array(solution, dtype=float)
+    return solution
+
+
+
+def gaussian_elimination(A,b):
+    n = len(b)
+    #forward elimnation
+    for i in range(n):
+        #Partial Pivot step
+        max_row = i + np.argmax(np.abs(A[i:n,i]))
+        if i != max_row:
+            A[[i, max_row]] = A[[max_row, i]] #switches rows A
+            b[[i, max_row]] = b[[max_row, i]] #switches rows b
+        for k in range(i+1, n):
+            factor = A[k, i] / A[i, i]
+            for j in range(i, n):
+                A[k, j] -= factor * A[i, j]
+                b[k] -= factor * b[i]
+    #backward
+    x = np.zeros(n)
+    for i in range(n-1, -1, -1):
+        #adds out of bounds condition
+        x[i] = (b[i] - np.dot(A[i, i+1:] if i+1 < n else 0, x[i+1:] if i+1 < n else 0)) / A[i, i]
+    return x
+
+n = int(input("What is the size of the N x N matrix? "))
+#print("Matrix:")
+#print(create_matrix(n))
+
+A = create_matrix(n)
+b = solution_matrix(n)
+# print(A)
+# print(b)
+# solution = gaussian_elimination(A,b)
+# print(solution)
