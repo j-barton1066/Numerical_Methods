c537cd097baa4820e4cdafdcf454bb40be27d96f
J Barton <jamesgoodinbarton@gmail.com>
2025-02-25 12:22:00 -0700

    Update Problem3.py

--- Assignment2/Problem3.py
+++ Assignment2/Problem3.py
@@ -7,7 +7,7 @@ def create_matrix(n):
     print(f"Enter the {n} x {n} matrix by rows: ")
     for i in range(n):
         while True:
-            row_list = list(map(int, input(f"Row {i+1}: ").split()))
+            row_list = list(map(float, input(f"Row {i+1}: ").split()))
             if len(row_list) == n:
                 break
             else:
@@ -15,21 +15,6 @@ def create_matrix(n):
         matrix.append(row_list)
     matrix = np.array(matrix, dtype=float)
     return matrix
-
-# def solution_matrix(n):
-#     solution = []
-#     print(f"Enter the {n} x 1 matrix by rows: ")
-#     for i in range(n):
-#         while True:
-#             row_list = list(map(int, input(f"Row {i+1}: ").split()))
-#             if len(row_list) == 1:
-#                 break
-#             else:
-#                 print("Please enter a valid matrix.")
-#         solution.append(row_list)
-#     solution = np.array(solution, dtype=float)
-#     return solution
-
 
 
 def crout(A):
@@ -42,23 +27,23 @@ def crout(A):
         for j in range(i, n):
             sum = 0
             for k in range(i):
-                sum = sum + (L[i][k] * U[k][j])
+                sum += (L[i][k] * U[k][j])
+            L[j][i] = A[j][i] - sum
+
         #Upper Triangle
-        for j in range(i+1, n):
+        for j in range(i, n):
             sum = 0
             for k in range(i):
-                sum = sum + (L[i][k] * U[k][j])
-            U[i][j] = (A[i][j] - sum)
+                sum += (L[i][k] * U[k][j])
+            U[i][j] = (A[i][j] - sum)/ L[i][i]
     return L, U
 
 def identity_matrix(n):
-    I = [[0] * n for _ in range(n)]
-    for i in range(n):
-        I[i][i] = 1
+    I = np.identity(n)
     return I
 
 def zero_matrix(n):
-    Z = [[0] for _ in range(n)]
+    Z = np.zeros((n,n))
     return Z
 
 def main():
@@ -68,16 +53,21 @@ def main():
     I = identity_matrix(n)
     Z = zero_matrix(n)
 
-    #
-    # A = create_matrix(n)
-    # print(f"The System to be solved is: ")
-    print(f"\nMatrix: \n{I}")
-    print(f"\nMatrix: \n{Z}")
-    #
-    #
-    # crout = crout(A)
-    #
-    # print(crout)
+    
+    A = create_matrix(n)
+    print(f"The System to be solved is: ")
+    print(f"\nMatrix: \n{A}")
+    L,U = crout(A)
+    print(f"\nLower Triangular Matrix: \n{L}")
+    print(f"\nUpper Triangular Matrix: \n{U}")
+
 
 if __name__ == "__main__":
     main()
+
+
+# Test Cases
+# # 3
+# [[2, -1, 1],
+# [-3, 3, 9],
+# [-1, 2, 4]],
